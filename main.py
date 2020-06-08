3365259eb707d6fdadaee12e0489c6f770153940
xier670 <811892063@qq.com>
2020-06-08 13:27:18 +0800

    TypeError: 'pygame.Surface' object is not callable 注意：screen.blit(background,(0,0))而非screen.blit(background(0,0))

--- main.py
+++ main.py
@@ -20,6 +20,88 @@ pygame.display.set_icon(picture)
 #设置背景图
 background=pygame.image.load("resources/image/background.png").convert_alpha()
 
+
+
+#子弹类
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,bullet_img,init_pos):
+        # 调用父类的初始化方法初始化Sprite的属性
+        pygame.sprite.Sprite.__init__(self)
+        self.image=bullet_img
+        self.rect=self.image.get_rect()
+        self.rect.midbottom=init_pos
+        self.speed=10
+
+    def move(self):
+        self.rect.top -= self.speed
+
+#敌机类
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self,enemy_img,enemy_down_imgs,init_pos):
+        # 调用父类的初始化方法初始化sprite的属性
+        pygame.sprite.Sprite.__init__(self)
+        self.image=enemy_img
+        self.rect=self.image.get_rect
+        self.rect.topleft=init_pos
+        self.down_imgs=enemy_down_imgs
+        self.speed=2
+        self.down_index=0
+
+    #敌机移动，边界判断及删除在游戏主循环里处理
+    def move(self):
+        self.rect.top +=self.speed
+
+
+#玩家飞机类
+class Player(pygame.sprite.Sprite):
+    def __init__(self,player_rect,init_pos):
+        #调用父类的初始化方法初始化sprite的属性
+        pygame.sprite.Sprite.__init__(self)
+        self.image=[] #用来存储玩家飞机图片的列表
+        for i in range(len(player_rect)):
+            self.image.append(player_rect[i].convert_alpha())
+
+        self.rect=player_rect[0].get_rect()
+        self.rect.topleft=init_pos
+        self.speed=8
+        self.bullets=pygame.sprite.Group()
+        self.img_index=0
+        self.is_hit=False
+
+    #发射子弹
+    def shoot(self,bullet_img):
+        bullet=Bullet(bullet_img,self.rect.midtop)
+        self.bullets.add(bullet)
+
+    #向上移动，需要判断边界
+    def moveUp(self):
+        if self.rect.top<=0:
+            self.rect.top=0
+        else:
+            self.rect.top-=self.speed
+
+    #向下移动，需要判断边界
+    def moveDown(self):
+        if self.rect.top>=SCREEN_HEIGHT-self.rect.height:
+            self.rect.top=SCREEN_HEIGHT-self.rect.height
+        else:
+            self.rect.top+=self.speed
+
+    #向左移动，需要判断边界
+    def moveLeft(self):
+        if self.rect.left <=0:
+            self.rect.left=0
+        else:
+            self.rect.left-=self.speed
+
+    #向右移动，需要判断边界
+    def moveRight(self):
+        if self.rect.left>=SCREEN_WIDTH-self.rect.width:
+            self.rect.left=SCREEN_WIDTH-self.rect.width
+        else:
+            self.rect.left+=self.speed
+
+
 def startGame():
     #游戏循环帧率
     clock=pygame.time.Clock()
